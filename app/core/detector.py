49c4a1e59bdc6803a693ae59cf96e2da0df814ca
gxgx3456 <gxgx3456@qq.com>
2026-09-24 20:44:53 +0800

    检测流程编排 / Detection pipeline

--- app/core/detector.py
+++ app/core/detector.py
@@ -97,5 +97,3 @@ def detect_with_cluster(engine_cfg, base_dir, paragraphs, params, master, progre
             if progress_cb:
                 progress_cb(done, total)
     return results
-
-# aigc-toolkit: file purpose marker
